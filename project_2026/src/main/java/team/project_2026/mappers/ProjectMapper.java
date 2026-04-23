148409e37dcf049ccd59b90c64489b74d47d4139
alikomuska <72194372+alikomuska@users.noreply.github.com>
2026-04-23 21:51:41 +0300

    Delete project_2026 directory

--- project_2026/src/main/java/team/project_2026/mappers/ProjectMapper.java
+++
@@ -1,7 +0,0 @@
-package team.project_2026.mappers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.project_2026.domain.Project;
-
-public interface ProjectMapper extends JpaRepository<Project, Long> {
-}
--- project_2026/src/main/java/team/project_2026/serivce/DeveloperService.java
+++
@@ -1,9 +0,0 @@
-package team.project_2026.serivce;
-
-import team.project_2026.domain.Developer;
-
-public interface DeveloperService {
-    public boolean isDeveloperPresent(Developer developer);
-    public void saveDeveloper(Developer developer);
-    public Developer findByUsername(String username);
-}
